6ffebd5fb704e6f52dc9f6db3a9085435bc7cc3c
Piotr Plenik <piotr.plenik@gmail.com>
2017-06-26 01:07:41 +0200

    Add additional task to reset state

--- src/main/java/pl/teamlab/shopchallenge/startup/InitialStateRunner.java
+++ src/main/java/pl/teamlab/shopchallenge/startup/InitialStateRunner.java
@@ -1,6 +1,6 @@
 /**
  * (c) Piotr Plenik <piotr.plenik@gmail.com>
- *
+ * <p>
  * For the full copyright and license information, please view the LICENSE
  * file that was distributed with this source code.
  */
@@ -17,10 +17,14 @@ import pl.teamlab.shopchallenge.repository.ItemDAO;
 public class InitialStateRunner implements CommandLineRunner {
     @Autowired
     private ItemDAO itemRepository;
-    
     @Override
     public void run(String... args) throws Exception {
         itemRepository.add(new Item(1L, "Item A", 20));
         itemRepository.add(new Item(2L, "Item B", 10));
+    }
+
+    public void clean() {
+        itemRepository.remove(1L);
+        itemRepository.remove(2L);
     }
 }
